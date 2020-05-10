609fdbb5f81b4707753de81ebc1aaf210c7da171
Donald Whitely <dswhitely1@gmail.com>
2020-05-09 22:20:28 -0400

    About Finished

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Don from 'assets/Don.png'
 import {Image} from "../styled-components/Image";
+import {AboutContainer, AboutMeContainer} from "../styled-components/AboutContainer";
+import {Button} from "../styled-components/Button";
 
 const About = () => (
-    <>
-        <Image src={Don} alt="Donald Whitely" />
-    </>
+    <AboutContainer>
+        <Image src={Don} alt="Donald Whitely"/>
+        <AboutMeContainer>
+            <h2>About Me</h2>
+            <p>I’m a junior front-end developer looking for a new role in an exciting company.
+                I focus on writing accessible HTML, using modern CSS practices and writing clean
+                JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to
+                whatever tools are required. I’m based in Corydon, IN, but I’m happy working remotely
+                and have experience in remote teams. When I’m not coding, you’ll find me spending time
+                with my family, or spending time in Azeroth.</p>
+            <Button secondary>Go to Portfolio</Button>
+        </AboutMeContainer>
+    </AboutContainer>
 )
 
 export default About;
---
+++ src/components/styled-components/AboutContainer.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+    display: flex;
+    margin-bottom: 15rem;
+`;
+
+export const AboutMeContainer = styled.div`
+    width: 35rem;
+    border-top: 1px solid #203A4C;
+    border-bottom: 1px solid #203A4C;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+`;
