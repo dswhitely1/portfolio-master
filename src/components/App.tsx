d94ec818bebbc8a6e15899429012d9f53166ac56
Donald Whitely <dswhitely1@gmail.com>
2020-05-10 18:09:53 -0400

    Switched to 100% Styled Components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,17 +5,21 @@ import {Route, Switch} from 'react-router-dom'
 import Navigation from "./nav/Navigation";
 import Footer from "./footer/Footer";
 import LandingPage from "./landing-page/LandingPage";
+import {GlobalStyle} from "./styled-components/GlobalStyle";
 
 const App = () => (
-    <Container>
-        <Wrapper>
-            <Navigation/>
-            <Switch>
-                <Route exact path="/" component={LandingPage}/>
-            </Switch>
-        </Wrapper>
-        <Footer/>
-    </Container>
-)
+    <>
+        <GlobalStyle/>
+        <Container>
+            <Wrapper>
+                <Navigation/>
+                <Switch>
+                    <Route exact path="/" component={LandingPage}/>
+                </Switch>
+            </Wrapper>
+            <Footer/>
+        </Container>
+    </>
+);
 
 export default App;
