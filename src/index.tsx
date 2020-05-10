e1b3aa3b692dbb7de70c47579948745d088d0ca8
Donald Whitely <dswhitely1@gmail.com>
2020-05-09 20:01:11 -0400

    Added Base Styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-import App from './components/App';
-
+import App from 'components/App';
+import 'styles/main.scss'
 
 const AppWithStrictMode = () => (<React.StrictMode><Router><App /></Router></React.StrictMode>);
 
