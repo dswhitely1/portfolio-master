10215ac378240686d407ae3668ccb6410b443a48
Donald Whitely <dswhitely1@gmail.com>
2020-05-09 22:06:40 -0400

    Added My Image

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,14 @@ import {Container} from "./styled-components/Container";
 import {Wrapper} from "./styled-components/Wrapper";
 import Navigation from "./nav/Navigation";
 import Hero from "./hero/Hero";
+import About from "./about/About";
 
 const App = () => (
     <Container>
         <Wrapper>
             <Navigation/>
             <Hero/>
+            <About/>
         </Wrapper>
     </Container>
 )
---
+++ src/components/about/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Don from 'assets/Don.png'
+import {Image} from "../styled-components/Image";
+
+const About = () => (
+    <>
+        <Image src={Don} alt="Donald Whitely" />
+    </>
+)
+
+export default About;
