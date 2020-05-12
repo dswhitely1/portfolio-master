9665f7fbb94d4f9c6012030797cd6ed66089ff20
Donald Whitely <dswhitely1@gmail.com>
2020-05-12 12:11:45 -0400

    Contact Button on Landing Page now going to Contact Page

--- src/components/landing-page/contact/Contact.tsx
+++ src/components/landing-page/contact/Contact.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import {ContactContainer, ContactHeader, Line} from "../../styled-components/Contact";
 import {Button} from "../../styled-components/Button";
 
-const Contact = () => (
+const Contact = () => {
+    const history = useHistory()
+    return (
     <ContactContainer>
         <ContactHeader>
             <h2>Interested in doing a project together?</h2>
         </ContactHeader>
         <Line/>
-        <Button secondary>Contact Me</Button>
+        <Button secondary onClick={() => history.push('/contact')}>Contact Me</Button>
     </ContactContainer>
-)
+)}
 
 export default Contact;
